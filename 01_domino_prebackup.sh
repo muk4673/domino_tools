39bed1735f564eff6ac5ba8fa918a9028e4000be
Markus Schmuck <35804488+muk4673@users.noreply.github.com>
2025-06-10 22:50:00 +0200

    added  for -loop with strings

--- 01_domino_prebackup.sh
+++ 01_domino_prebackup.sh
@@ -1,37 +1,56 @@
-#!/bin/sh
+#!/bin/bash
 
-CopyDir=/tmp/domino101_prebackup
+VERS=$1
+
+NOTES_ADMIN_FILES="admin4.nsf catalog.nsf certlog.nsf ddm.nsf events4.nsf inetlockout.nsf names.nsf ressourcen.nsf webadmin.nsf"
+
+NOTES_CONFIG_FILES="bookmark.nsf busytime.nsf cldbusy.nsf da.nsf domcfg.nsf homepage.nsf schema.nsf"
+
+NOTES_MAIL_FILES="mail.box mail1.box mail2.box mail3.box"
+
+if [ ! -e "/local/backup" ]; then
+    mkdir -p /local/backup
+    chown notes:notes /local/backup
+    chmod 770 /local/backup
+fi
+
+CopyDir=/local/backup/$VERS
 ProgramDir=/opt/ibm/domino/
-DataDir=/notesdata
+DataDir=/local/notesdata
+
+mkdir -p $CopyDir
+chown notes:notes $CopyDir
+chmod 770 $CopyDir
 
 echo "***"
 echo "Sicherung der systemnahen Datenbanken startet"
 echo "***"
 echo ""
 
-mkdir $CopyDir
-
 cp $DataDir/*.ntf $CopyDir/
 cp $DataDir/*.id $CopyDir/
 cp $DataDir/*.sid $CopyDir/
-cp $DataDir/admin4.nsf $CopyDir/
-cp $DataDir/bookmark.nsf $CopyDir/
-cp $DataDir/busytime.nsf $CopyDir/
-cp $DataDir/catalog.nsf $CopyDir/
-cp $DataDir/certlog.nsf $CopyDir/
-cp $DataDir/cldbdir.nsf $CopyDir/
-cp $DataDir/clubusy.nsf $CopyDir/
-cp $DataDir/da.nsf $CopyDir/
-cp $DataDir/ddm.nsf $CopyDir/
-cp $DataDir/domcfg.nsf $CopyDir/
-cp $DataDir/events4.nsf $CopyDir/
-cp $DataDir/homepage.nsf $CopyDir/
-cp $DataDir/inetlockout.nsf $CopyDir/
-cp $DataDir/mail.box $CopyDir/
-cp $DataDir/names.nsf $CopyDir/
-cp $DataDir/ressourcen.nsf $CopyDir/
-cp $DataDir/schema.nsf $CopyDir/
-cp $DataDir/webadmin.nsf $CopyDir/
+
+for db in $NOTES_ADMIN_FILES
+do
+    if [ -e $DataDir/"$db" ]; then
+        cp $DataDir/"$db" $CopyDir/
+    fi
+done
+
+for db in $NOTES_CONFIG_FILES
+do
+    if [ -e $DataDir/"$db" ]; then
+        cp $DataDir/"$db" $CopyDir/
+    fi
+done
+
+for db in $NOTES_MAIL_FILES
+do
+    if [ -e $DataDir/"$db" ]; then
+        cp $DataDir/"$db" $CopyDir/
+    fi
+done
 
 echo "***"
 echo "Sicherung der systemnahen Datenbanken abgeschlossen"
